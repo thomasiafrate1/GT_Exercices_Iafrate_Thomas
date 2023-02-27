a69654e9f10eba7e053e6b21e0fe53cb867f7f70
thomasiafrate1 <113009615+thomasiafrate1@users.noreply.github.com>
2023-02-27 13:37:40 +0100

    GO-2-FICHIER

--- EXERCICE GOLANG/2-FICHIERS/main.go
+++ EXERCICE GOLANG/2-FICHIERS/main.go
@@ -1,39 +1,103 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"os"
 )
 
 func main() {
-	AjouterTxt()
+	for {
+
+		fmt.Println(" _________________________________________")
+		fmt.Println("|                 MENU:                   |")
+		fmt.Println("|         1. Lire le fichier              |")
+		fmt.Println("|         2. Ajouter du texte             |")
+		fmt.Println("|         3. Supprimer le texte           |")
+		fmt.Println("|         4. Remplacer le texte           |")
+		fmt.Println("|         5. Quitter                      |")
+		fmt.Println("|_________________________________________|")
+
+		var choice int
+		fmt.Print("Choisissez une option :")
+		fmt.Scanln(&choice)
+
+		switch choice {
+		case 1:
+			lireTxt()
+		case 2:
+			AjouterTxt()
+		case 3:
+			SupprimerTxt()
+		case 4:
+			RemplacerTxt()
+		case 5:
+			fmt.Println("Au revoir")
+			return
+		default:
+			fmt.Println("Mauvais choix, réesayez avec un nombre entre 1 et 5.")
+		}
+	}
 }
 
 func lireTxt() {
-	file, err := ioutil.ReadFile("EXERCICE GOLANG/2-FICHIERS/test.txt") // lire le fichier text.txt
+	file, err := ioutil.ReadFile("EXERCICE GOLANG/2-FICHIERS/test.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(string(file)) // conversion de byte en string
+	fmt.Println(string(file))
 }
 
 func AjouterTxt() {
-	var crit string
+	var ecrit string
 
-	fmt.Print("choisissez un truc à écrire :")
-	fmt.Scanln(&crit)
-	file, err := os.OpenFile("EXERCICE GOLANG/2-FICHIERS/test.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600){
-		panic(err)
-	}
-	_, err = file.WriteString(crit) // écrire dans le fichier
+	fmt.Print("Choisissez quelque chose à dire :")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	ecrit = scanner.Text()
+	file, err := os.OpenFile("EXERCICE GOLANG/2-FICHIERS/test.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
 	if err != nil {
 		panic(err)
 	}
-	data, err := ioutil.ReadFile("EXERCICE GOLANG/2-FICHIERS/test.txt") 
+	_, err = file.WriteString(ecrit)
+	if err != nil {
+		panic(err)
+	}
+	data, err := ioutil.ReadFile("EXERCICE GOLANG/2-FICHIERS/test.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Print(string(data))
+}
+
+func SupprimerTxt() {
+	monfichier := "EXERCICE GOLANG/2-FICHIERS/test.txt"
+
+	err := ioutil.WriteFile(monfichier, []byte(""), 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("Le fichier %s a été vidé\n", monfichier)
+}
+
+func RemplacerTxt() {
+	monfichier := "EXERCICE GOLANG/2-FICHIERS/test.txt"
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Entrez le nouveau contenu du fichier : ")
+	nouveauTxt, _ := reader.ReadString('\n')
+
+	err := ioutil.WriteFile(monfichier, []byte(nouveauTxt), 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("Le fichier %s a été mis à jour\n", monfichier)
 }
