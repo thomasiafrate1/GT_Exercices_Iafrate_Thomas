6bde4ed93351ceae158be9b0c102f4be49a70760
thomasiafrate1 <113009615+thomasiafrate1@users.noreply.github.com>
2023-02-27 14:26:58 +0100

    GO-3-TABLEAUX

---
+++ EXERCICE GOLANG/3-TABLEAUX/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"html/template"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	x := rand.Intn(13) + 3
+	y := rand.Intn(13) + 3
+	tableau := make([][]int, y)
+	for i := range tableau {
+		tableau[i] = make([]int, x)
+		for j := range tableau[i] {
+			tableau[i][j] = rand.Intn(100) + 1
+		}
+	}
+	tmpl, err := template.ParseFiles("EXERCICE GOLANG/3-TABLEAUX/TABLEAUX.html")
+	if err != nil {
+		panic(err)
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err := tmpl.Execute(w, tableau)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
